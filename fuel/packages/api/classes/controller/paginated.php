6d747b309d56c9bcecbcfef26c0ecbe7c2077326
Tom Veldman <tom.veldman@live.nl>
2021-01-13 22:23:00 +0100

    Add improved API package from RSK site
    Taken from https://github.com/PeterTillema/rsk-sitev2/tree/master/fuel/packages/api

--- fuel/packages/api/classes/controller/paginated.php
+++ fuel/packages/api/classes/controller/paginated.php
@@ -3,10 +3,11 @@
 namespace Api;
 
 /**
- * Enables pagination on API controllers. <br>
- * This controller accepts request in the form of <br>
- * <b>/$endpoint?search={text}&sort={property/column}&limit=x&offset=y&order=(asc|desc)</b><br><br>
+ * Enables pagination on API controllers. This controller accepts request in the form of <br>
+ * <b>/$endpoint?search={text}&sort={property/column}&limit=x&offset=y&order=(asc|desc)</b>
+ * <br><br>
  * To generate paginated output, explicitly return data using the Response_Paginated wrapper.
+ * @author Melcher
  */
 class Controller_Paginated extends Controller_Auth {
 	
@@ -27,12 +28,12 @@ class Controller_Paginated extends Controller_Auth {
 	 * @var string Either 'asc' for ascending or 'desc' for descending order. 
 	 */
 	protected $order = 'asc';
-			
+	
 	/**
-	 * Column on which to sort. Default is id.
-	 * @var string 
+	 * Secondary sorting column.
+	 * @var string
 	 */
-	protected $sort;
+	protected $sort_secondary;
 	
 	/**
 	 * Search string. 
@@ -40,63 +41,130 @@ class Controller_Paginated extends Controller_Auth {
 	 */
 	protected $search;
 	
-	private static $_default_sort_key = 'id';
+	protected $default_sort_key = 'id';
 	
 	/**
-	 * Columns that will be ignored when defined as sorting key.
+	 * Columns that will be ignored when defined as sorting key. <br>
+	 * Essentially acts as a blacklist.
 	 * @var array 
 	 */
 	protected $unsortable_columns = [];
 	
+	/**
+	 * Whitelist of sortable columns. If not set, all columns can be sorted <br>
+	 * with exception those in the `$unsortable_columns` array.
+	 * @var array 
+	 */
+	protected $sortable_columns;
+	
+	/**
+	 * Columns that will be searched when a search query is executed.
+	 * @var array 
+	 */
+	protected $searchable_columns;
+	
 	public function before() {
 		parent::before();
 		
-		/* 
-		 * Set pagination values based on query - or use defaults instead.
-		 */		
+		// Set pagination values based on query - or use defaults instead.	
 		$this->offset = \Input::get('offset', 0);
 		$this->limit = \Input::get('limit', PHP_INT_MAX);
-		$this->sort = \Input::get('sort', self::$_default_sort_key);
 		$this->search = \Input::get('search');
 		
 		// Only set order when appropriate
 		if(in_array(($order_param = \Input::get('order')), ['asc', 'desc'])){
 			$this->order =$order_param;
 		}	
-		
-		// Reset sort key whenever it was defined as an unsortable column. 
-		if(in_array($this->sort, $this->unsortable_columns)) {
-			$this->sort = self::$_default_sort_key;
-		}
 	}
 	
 	/**
 	 * Execute given query with the pagination details as requested.
-	 * @param \Orm\Query $query The query
-	 * @return array [query result array, total records]
+	 * @param mixed $query The query
+	 * @return array [$raw_query_results, $tota_row_count]
 	 */
-	protected final function paginate_query(\Orm\Query $query) : array {
+	protected final function paginate_query($query) : array {
+		// Determine sort here instead of in before() method. This solves 
+		// loads of problems
+		$sort = \Input::get('sort', $this->default_sort_key);
+		
 		/* Apply search query to this SQL query. 
 		Searches only apply on the currently sorted column defined by 'sort'. */
 		if(isset($this->search) && ($this->search != '')) {
-			$query = $query->where($this->sort, 'like', '%' . $this->search . '%');
+			$query->where_open();
+
+			// Default behavior: search on sorted column
+			$query = $query->where($sort, 'LIKE', '%' . $this->search . '%');
+			
+			# Perform additional searching			
+			if (!empty($this->searchable_columns)) {
+				$search_cols = $this->searchable_columns;
+			} else if (!empty($this->sortable_columns)) {
+				$search_cols = $this->sortable_columns;
+			} 
+			
+			if (!empty($search_cols)) {
+				$query->or_where_open();
+				foreach ($search_cols as $col) {
+					$query->or_where($col, 'LIKE', '%' . $this->search . '%');
+				}
+				$query->or_where_close();
+			}
+
+			$query->where_close();
 		}
 		
-		$count = $query->count();
-		$pagQuery = $query
-				->rows_offset($this->offset)
-				->rows_limit($this->limit)
-				->order_by($this->sort, $this->order);
+		// Reset sort key whenever it was defined as an unsortable column. 
+		if(in_array($sort, $this->unsortable_columns)) {
+			$sort = $this->default_sort_key;
+		}
 		
-		/* Catch faulty queries which may be created 
-		by using non-existant properties as sort key */
+		// Reset sort key whenever it was not explicitly defined as sortable.
+		if(!empty($this->sortable_columns) && !in_array($sort, $this->sortable_columns)) {
+			$sort = $this->default_sort_key;
+		}
+		
+		// Apply order
+		$query->order_by($sort, $this->order);
+		
+		// Apply second order. This prevents unexpected behavior for some queries.
+		if(isset($this->sort_secondary) && trim($this->sort_secondary) != '') {
+			$query->order_by($this->sort_secondary);
+		}
+		
+		// Apply query type specific actions
 		try {
-			$result = $pagQuery->get();
+			if($query instanceof \Orm\Query) {
+				$count = $query->count();
+				$query
+					->rows_offset($this->offset)
+					->rows_limit($this->limit);			
+				$result = $query->get();
+			} else if($query instanceof \Database_Query_Builder_Select) {					
+				$query
+					->offset($this->offset)
+					->limit($this->limit);			
+				$result = $query->execute()->as_array();
+				$count = \DB::count_last_query();
+			} else {
+				throw new FuelException("Query type unsupported for pagination");
+			}	
 		} catch (\Fuel\Core\Database_Exception $ex) {
-			// Caught
+			/* Catch faulty queries which may be created 
+			by using non-existant properties as sort key */
 			$result = [];
+			$count = 0;
+			throw $ex;
 		}		
-				
 		return [$result, $count];
+	}
+	
+	/**
+	 * Paginate query and fetch into Response_Paginated class.
+	 * @param mixed $query
+	 * @return \Api\Response_Paginated
+	 */
+	protected final function paginate_fetch_into($query) : Response_Paginated {
+		$arr = $this->paginate_query($query);
+		return new Response_Paginated($arr[0], $arr[1]);
 	}
 }
