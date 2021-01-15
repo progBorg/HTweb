56ea7221fe789fcb47cb6a93911fc1641636a545
Tom Veldman <tom.veldman@live.nl>
2021-01-15 15:16:55 +0100

    Trigger notification gathering before processing

--- fuel/app/classes/controller/notifications/dropdown.php
+++ fuel/app/classes/controller/notifications/dropdown.php
@@ -1,24 +1,27 @@
 <?php
 
+use Fuel\Core\Event;
+use Fuel\Core\Request;
+use Fuel\Core\View;
+
 class Controller_Notifications_Dropdown extends Controller_Core_Lang {
 	
 	public function before() {
-		if (!\Request::is_hmvc()) {
+		if (!Request::is_hmvc()) {
 			return;
 		}
 		parent::before();
 	}
 	
 	public function get_index() {
+		Event::trigger('gather_notifications');
+
 		$user_id = $this->current_user->id;
 		$count = \Model_Notification::get_unread_count($user_id);
 		$notifications = \Model_Notification::get_unread($user_id) ? : [];
-		
-		\Event::trigger('gather_notifications');		
 			
-		return \View::forge('notifications/dropdown', 
+		return View::forge('notifications/dropdown',
 				['unread_count' => $count > 0 ? $count : '', 'notifications' => $notifications]);
 	}
-	
-	
+
 }
